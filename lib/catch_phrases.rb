e859fcc0e961da6691b00e2054b1eb5452c65e7c
Wesley Chen <weschen1996@gmail.com>
2020-01-17 07:21:44 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,7 +4,7 @@ def mario
 end
 
 def toadstool
-  phrase = 'Thank You Mario! But Our Princess Is In Another Castle!d'
+  phrase = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
