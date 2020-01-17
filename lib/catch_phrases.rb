5b9813250694cadefced091b96621bb5ec4713dd
Wesley Chen <weschen1996@gmail.com>
2020-01-17 07:21:30 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,10 @@
+def mario
+  status = "it's-a me, Mario!d"
+  puts status
+end
+
 def toadstool
   phrase = 'Thank You Mario! But Our Princess Is In Another Castle!\n'
   puts phrase
 end
 
-def mario
-  status = "it's-a me, Mario!\n"
-  puts status
-end
